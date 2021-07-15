31fcab8252e31ae63e75930717391b74dd604f11
sandalaj <jennasandala@gmail.com>
2021-07-15 15:38:22 +0000

    update app files

--- app.r
+++ app.r
@@ -1,102 +1,120 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
-# Define UI for data upload app ----
+# Define UI for application that draws a histogram
 ui <- fluidPage(
 
-  # App title ----
-  titlePanel("Uploading Files"),
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
 
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
 
-    # Sidebar panel for inputs ----
-    sidebarPanel(
+            
+            # Input: Select a file ----
+            fileInput("file1", "Choose CSV File",
+                      multiple = FALSE,
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv")),
+            
+            # Horizontal line ----
+            tags$hr(),
+            
+            # Input: Checkbox if file has header ----
+            checkboxInput("header", "Header", TRUE),
+            
+            # Input: Select separator ----
+            radioButtons("sep", "Separator",
+                         choices = c(Comma = ",",
+                                     Semicolon = ";",
+                                     Tab = "\t"),
+                         selected = ","),
+            
+            # Input: Select quotes ----
+            radioButtons("quote", "Quote",
+                         choices = c(None = "",
+                                     "Double Quote" = '"',
+                                     "Single Quote" = "'"),
+                         selected = '"'),
+            
+            # Horizontal line ----
+            tags$hr(),
+            
+            # Input: Select number of rows to display ----
+            radioButtons("disp", "Display",
+                         choices = c(Head = "head",
+                                     All = "all"),
+                         selected = "head")
+        ),
 
-      # Input: Select a file ----
-      fileInput("file1", "Choose CSV File",
-                multiple = FALSE,
-                accept = c("text/csv",
-                         "text/comma-separated-values,text/plain",
-                         ".csv")),
-
-      # Horizontal line ----
-      tags$hr(),
-
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
-
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
-
-      # Input: Select quotes ----
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
-
-      # Horizontal line ----
-      tags$hr(),
-
-      # Input: Select number of rows to display ----
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head")
-
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Data file ----
-      tableOutput("contents")
-
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot"),
+           plotOutput("lmPlot"),
+           tableOutput("contents")
+        )
     )
-
-  )
 )
 
-# Define server logic to read selected file ----
+# Define server logic required to draw a histogram
 server <- function(input, output) {
 
-  output$contents <- renderTable({
-
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-
-    req(input$file1)
-
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
-    tryCatch(
-      {
+    dataInput <- reactive({
+        req(input$file1)
+        
         df <- read.csv(input$file1$datapath,
-                 header = input$header,
-                 sep = input$sep,
-                 quote = input$quote)
-      },
-      error = function(e) {
-        # return a safeError if a parsing error occurs
-        stop(safeError(e))
-      }
-    )
-
-    if(input$disp == "head") {
-      return(head(df))
-    }
-    else {
-      return(df)
-    }
-
-  })
-
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+        return(df)
+    })
+    
+    # output$distPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #     x    <- faithful[, 2]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #     print(bins)
+    #     # draw the histogram with the specified number of bins
+    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # })
+    # 
+    
+    output$distPlot <- renderPlot({
+        plot(dataInput()$x,dataInput()$y)
+    })
+    
+    output$lmtPlot <- renderPlot({
+        plot(dataInput()$x,dataInput()$y)
+    })
+    
+    
+    output$contents <- renderTable({
+        
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, head of that data file by default,
+        # or all rows if selected, will be shown.
+        
+        
+        if(input$disp == "head") {
+            return(head(dataInput()))
+        }
+        else {
+            return(dataInput())
+        }
+        
+    })
+        
 }
 
-# Create Shiny app ----
-shinyApp(ui, server)
+# Run the application 
+shinyApp(ui = ui, server = server)
