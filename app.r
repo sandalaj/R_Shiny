aef73e95d2f12a5ca9b9cc3717465516913bd9f1
sandalaj <jennasandala@gmail.com>
2021-07-22 09:25:40 +0000

    fixed

--- app.r
+++ app.r
@@ -14,14 +14,14 @@ library(ggplot2)
 
 # Define UI for application
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("Linear Modeling"),
-
+    
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-
+            
             
             # Input: Select a file ----
             fileInput("file1", "Choose CSV File",
@@ -59,25 +59,19 @@ ui <- fluidPage(
                                      All = "all"),
                          selected = "head"),
             
-            #Create action button to plot the scatterplot
-            actionButton("scatter", "Plot scatterplot"),
             
             #Create action button to plot linear model and associated data values
-            actionButton("lm", "Plot linear model")
+            actionButton("plotData", "Plot scatterplot"),
+            actionButton("addModel", "Plot linear model")
         ),
         
-           
-
+        
+        
         # Plotting the scatterplot and associated information
         mainPanel(
-           plotOutput("distPlot"),
-           plotOutput("lmPlot"),
-           textOutput("R-squared Values"),
-           textOutput("rsquared"),
-           textOutput("intercept"),
-           textOutput("slope"),
-           textOutput("pvalue"),
-           tableOutput("contents")
+            plotOutput("scatter"),
+            plotOutput("lmod"),
+            tableOutput("contents")
         )
     )
 )
@@ -85,7 +79,7 @@ ui <- fluidPage(
 
 # Define server logic 
 server <- function(input, output) {
-
+    
     
     dataInput <- reactive({
         req(input$file1)
@@ -97,56 +91,37 @@ server <- function(input, output) {
         return(df)
     })
     
-    #Action button for linear model
-    lmPlot <- eventReactive(input$lm, {
+    scatterplot <- eventReactive(input$plotData, {
         lm(y ~ x, data = dataInput())
+        
     })
     
-    #Action button for scatterplot
-     distPlot <- eventReactive(input$scatter, {
-         plot(x = dataInput()$x, y = dataInput()$y, main = "Scatterplot")
-         lm(y ~ x, data = dataInput())
-    })   
-
-     output$distPlot <- renderPlot({
-         ggplot(distPlot()$model, aes_string(x = names(distPlot()$model)[2], y = names(distPlot()$model)[1])) + 
-             geom_point() +
-             ggtitle("Scatterplot") +
-             theme(plot.title = element_text(hjust = 0.5, face = "bold"))
-     })
-    
-
-
-    
-    # Create outputs for slope, intercept, correlation coefficient, and p-value for the linear model
-    output$lmPlot <- renderPlot({
-        ggplot(lmPlot()$model, aes_string(x = names(lmPlot()$model)[2], y = names(lmPlot()$model)[1])) + 
-            geom_point() +
-            stat_smooth(method = "lm", col = "blue") +
-            ggtitle("Linear Model") +
+    output$scatter <- renderPlot( {
+        ggplot(scatterplot()$model, aes_string(x = names(scatterplot()$model)[2], y = names(scatterplot()$model)[1])) + 
             theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
-            geom_label(aes(x = 0, y = 7.5), hjust = 0, 
-                       label = paste("Adj R2 = ",signif(summary(lmPlot())$adj.r.squared, 5),
-                                     "\nIntercept =",signif(lmPlot()$coef[[1]],5 ),
-                                     " \nSlope =",signif(lmPlot()$coef[[2]], 5),
-                                     " \nP =",signif(summary(lmPlot())$coef[2,4], 5)))
+            geom_point() +
+            ggtitle("Scatterplot") 
+        
     })
     
-    # Label the graph with the generated values for slope, intercept, correlation coeffcient, and p-value
-    output$rsquared <- renderText({
-        print(paste("Adj R2 = ",signif(summary(lmPlot())$adj.r.squared, 5)))
+    
+    linmod <- eventReactive(input$addModel, {
+        lm(y ~ x, data = dataInput())
+        
     })
     
-    output$intercept <- renderText({
-        print(paste("Intercept =",signif(lmPlot()$coef[[1]],5)))
-    })
-    
-    output$slope <- renderText({
-        print(paste("Slope =",signif(lmPlot()$coef[[2]], 5)))
-    })
-    
-    output$pvalue <- renderText({
-        print(paste("P =",signif(summary(lmPlot())$coef[2,4], 5)))
+    output$lmod <- renderPlot( {
+        ggplot(linmod()$model, aes_string(x = names(linmod()$model)[2], y = names(linmod()$model)[1])) + 
+            theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+            geom_point() +
+            ggtitle("Scatterplot with Linear Model") +
+            stat_smooth(method = "lm", col = "blue") +
+            geom_label(aes(x = 0, y = 7.5), hjust = 0,
+                label = paste("Adj R2 = ",signif(summary(linmod())$adj.r.squared, 5),
+                              " \nIntercept =",signif(linmod()$coef[[1]],5 ),
+                              " \nSlope =",signif(linmod()$coef[[2]], 5),
+                              " \nP =",signif(summary(linmod())$coef[2,4], 5)))
+            
     })
     
     # Show the table of plotted x and y values
@@ -160,8 +135,9 @@ server <- function(input, output) {
         }
         
     })
-        
+    
 }
 
 # Run the application 
+
 shinyApp(ui = ui, server = server)
